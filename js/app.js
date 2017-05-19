7d7e7be04373e1c23803e50a1f29ade84c973dfc
Thibaud BARDIN <thibaud.bardin@gmail.com>
2017-05-19 15:07:51 +0200

    update

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// Trainers
+    // Name
+    // Pokemons
+        // Name
+        // HP
+        // Attack
+        // Type
+
+const trainers = [
+    {
+        name: "Sasha",
+        pokemons: [
+            { name: "Pikachu", hp: 100, attack: 30, type: "electric" },
+            { name: "Salameche", hp: 115, attack: 20, type: "fire" },
+            { name: "Secateur", hp: 80, attack: 17, type: "plant" }
+        ]
+    },
+    {
+        name: "Pierre",
+        pokemons: [
+            { name: "Racaillou", hp: 100, attack: 18, type: "rock" },
+            { name: "Onyx", hp: 200, attack: 20, type: "rock" },
+            { name: "Magicarpe", hp: 500, attack: 1, type: "water" }
+        ]
+    },
+    {
+        name: "Regis",
+        pokemons: [
+            { name: "Florizarre", hp: 300, attack: 28, type: "plant" },
+            { name: "Tortank", hp: 250, attack: 31, type: "water" },
+            { name: "Dracaufeu", hp: 280, attack: 29, type: "feu" }
+        ]
+    }
+];
+
+
